580a583853a14606dbb77538f3bf015b8acc1189
Abdelnasser Hajihashemabad <info.hashemabadi@gmail.com>
2023-02-26 11:39:35 +0000

    Create PHP_ConnectionFunction.php

---
+++ PHP_ConnectionFunction.php
@@ -0,0 +1,7 @@
+function my_custom_token_transaction() {
+   // Put MyTokenSender and MultiSigWallet contract code here
+
+   // Put the wallet connection function here
+}
+
+add_action('init', 'my_custom_token_transaction');
